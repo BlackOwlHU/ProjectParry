09ad12cc6d6c5f6443243cf42f87ac6720b81b48
BlackOwlHU <rolnadmester@gmail.com>
2024-09-10 11:41:25 +0200

    Baisc menu added
    Alap játék menü elemek nélkül.

---
+++ scenes/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
