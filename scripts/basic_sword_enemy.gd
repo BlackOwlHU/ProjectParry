16d0617d6aeb34494da39438e0f555ba4d2ae12b
BlackOwlHU <rolnadmester@gmail.com>
2024-09-18 20:19:49 +0200

    God Save Me Part1

--- scripts/basic_sword_enemy.gd
+++ scripts/basic_sword_enemy.gd
@@ -4,12 +4,13 @@ extends CharacterBody3D
 @onready var nav_agent = $NavigationAgent3D
 @export var SPEED = 3.0
 
+
 func _physics_process(delta: float) -> void:
 	var current_location = global_transform.origin
 	var next_location = nav_agent.get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
-	velocity = new_velocity
+	velocity = velocity.move_toward(new_velocity, .25)
 	move_and_slide()
 
 func update_target_location(target_location) -> void :
