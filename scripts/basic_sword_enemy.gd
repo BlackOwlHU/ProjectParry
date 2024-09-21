60c2d61be86c433286e1f4ce99dc42d075c82037
BlackOwlHU <rolnadmester@gmail.com>
2024-09-21 14:09:09 +0200

    idk what am i doing

--- scripts/basic_sword_enemy.gd
+++ scripts/basic_sword_enemy.gd
@@ -1,17 +1,13 @@
 extends CharacterBody3D
-
+#hours i have spend to fix this: 5
 @onready var ai = true
 @onready var nav_agent = $NavigationAgent3D
 @export var SPEED = 3.0
 @onready var player = get_tree().get_first_node_in_group("Player")
-
-func update():
-	var i = 1
-	while i > 0:
-		update_target_location() 
+@onready var timer = $Timer
 
 func _physics_process(delta: float) -> void:
-	look_at(player.position)
+	look_at(player.global_transform.origin)
 	var current_location = global_transform.origin
 	var next_location = nav_agent.get_next_path_position()-global_position
 	next_location.normalized()
@@ -19,5 +15,9 @@ func _physics_process(delta: float) -> void:
 	velocity = velocity.move_toward(new_velocity, .25)
 	move_and_slide()
 
-func update_target_location() -> void :
+func update_target_location() -> void:
 	nav_agent.target_position = player.global_transform.origin
+
+
+func _on_timer_timeout() -> void:
+	update_target_location()
