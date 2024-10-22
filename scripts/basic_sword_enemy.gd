49efe07c22cadd99058b966b7a9c909e03539a9e
BlackOwlHU <rolnadmester@gmail.com>
2024-10-22 12:18:35 +0200

    sobeit

--- scripts/basic_sword_enemy.gd
+++ scripts/basic_sword_enemy.gd
@@ -5,6 +5,7 @@ extends CharacterBody3D
 @export var SPEED = 3.0
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var timer = $Timer
+@onready var pos = $Position
 
 func _ready():
 	pass
@@ -21,20 +22,21 @@ func _physics_process(_delta: float) -> void:
 	if nav_agent.is_navigation_finished():
 		return
 	look_at(player.global_transform.origin+Vector3(0,1,0))
-	var current_location = global_transform.origin
+	var current_location = pos.global_transform.origin
 	var next_location = nav_agent.get_next_path_position()-global_position
 	#print(nav_agent.is_target_reachable())
 	var new_velocity = (next_location - current_location).normalized() * SPEED
 	nav_agent.set_velocity(new_velocity)
-	velocity = current_location.direction_to(next_location) * SPEED
+	velocity = (current_location.direction_to(next_location)) * SPEED
 	move_and_slide()
 
-func update_target_location(player: Vector3) -> void:
-	nav_agent.target_position = player
+func update_target_location():
+	nav_agent.target_position = player.position
+	print(nav_agent.target_position)
 
 func _on_timer_timeout() -> void:
 	#print("refresh")
-	update_target_location(player.position)
+	update_target_location()
 	timer.start()
 
 func _on_navigation_agent_3d_target_reached() -> void:
