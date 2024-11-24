dd2c80e4dbab75f6a941c6ffd8433fbb2e340f45
BlackOwlHU <rolnadmester@gmail.com>
2024-11-24 17:41:14 +0100

    a0.2
    fixed enemy ai following issue FINALLY

--- scripts/basic_sword_enemy.gd
+++ scripts/basic_sword_enemy.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 #hours i have spend to fix this: 9
 @onready var ai = true
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
-@export var SPEED = 3.0
+@export var SPEED = 5.0
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var timer = $Timer
 @onready var pos = $Position
@@ -23,11 +23,12 @@ func _physics_process(_delta: float) -> void:
 		return
 	look_at(player.global_transform.origin+Vector3(0,1,0))
 	var current_location = pos.global_transform.origin
-	var next_location = nav_agent.get_next_path_position()-global_position
+	var next_location = nav_agent.get_next_path_position()
+	next_location.y=current_location.y
 	#print(nav_agent.is_target_reachable())
-	var new_velocity = (next_location - current_location).normalized() * SPEED
+	var new_velocity =  (current_location.direction_to(next_location)) * SPEED
 	nav_agent.set_velocity(new_velocity)
-	velocity = (current_location.direction_to(next_location)) * SPEED
+	velocity = nav_agent.velocity
 	move_and_slide()
 
 func update_target_location():
