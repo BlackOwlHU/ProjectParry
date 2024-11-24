c8a05de76988c4995dbb178cda1cd80289081c3d
BlackOwlHU <rolnadmester@gmail.com>
2024-11-24 17:14:57 +0100

    a0.1

--- world.gd
+++ world.gd
@@ -3,5 +3,5 @@ extends Node3D
 @onready var player = $Player
 
 func _physics_process(_delta: float) -> void:
-	get_tree().call_group("enemy", "update_tart_location", player.global_position)
+	get_tree().call_group("enemy", "update_target_location", player.global_position)
 	
